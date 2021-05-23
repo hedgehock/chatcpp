1c9d2dee6421d4a3c2c5915699331f6775c30502
TheHedgehock <tomas.hedgehock@gmail.com>
2021-05-23 19:52:18 +0200

    upload

---
+++ Client/src/Main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <thread>
+#include <enet/enet.h>
+
+using namespace std;
+
+string username;
+
+void MsgLoop(ENetHost* _client)
+{
+	while (true)
+	{
+		ENetEvent event;
+		while (enet_host_service(_client, &event, 0) > 0)
+		{
+			switch (event.type)
+			{
+			case ENET_EVENT_TYPE_RECEIVE:
+				std::cout << event.packet->data << std::endl;
+				enet_packet_destroy(event.packet);
+				break;
+			}
+		}
+	}
+}
+
+void SendPacket(ENetPeer* _peer, const char* _data)
+{
+	ENetPacket* packet = enet_packet_create(_data, strlen(_data) + 1, ENET_PACKET_FLAG_RELIABLE);
+	enet_peer_send(_peer, 0, packet);
+}
+
+int main()
+{
+	cout << "Enter your username: ";
+	cin >> username;
+
+	if (enet_initialize() != 0)
+		std::cout << "Can't init ENet!" << std::endl;
+
+	ENetHost* client;
+	ENetAddress address;
+	ENetEvent event;
+	ENetPeer* peer;
+
+	client = enet_host_create(NULL, 1, 1, 0, 0);
+	enet_address_set_host(&address, "127.0.0.1");
+	address.port = 8080;
+	peer = enet_host_connect(client, &address, 1, 0);
+
+
+	if (enet_host_service(client, &event, 5000) > 0 && event.type == ENET_EVENT_TYPE_CONNECT)
+		cout << "Connection succeeded." << endl;
+	else
+	{
+		enet_peer_reset(peer);
+		cout << "Connection failed." << endl;
+	}
+
+	thread msgLoopThread(MsgLoop, client);
+
+	while (true)
+	{
+		string input;
+		cin >> input;
+
+		string message = username + " > " + input;
+
+		SendPacket(peer, message.c_str());
+	}
+
+	msgLoopThread.join();
+}
---
+++ Server/src/Main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <list>
+#include <enet/enet.h>
+
+using namespace std;
+
+list<ENetPeer*> peers;
+
+void SendPackets(ENetEvent _event)
+{
+	cout << _event.packet->data << std::endl;
+
+	for (ENetPeer* peer : peers)
+	{
+		ENetPacket* packet = enet_packet_create(_event.packet->data, _event.packet->dataLength, ENET_PACKET_FLAG_RELIABLE);
+		enet_peer_send(peer, 0, packet);
+	}
+}
+
+int main()
+{
+	if (enet_initialize() != 0)
+		cout << "Can't init enet!" << endl;
+
+	ENetAddress address;
+	ENetHost* server;
+	ENetEvent event;
+
+	address.host = ENET_HOST_ANY;
+	address.port = 8080;
+	server = enet_host_create(&address, 10, 1, 0, 0);
+
+	cout << "Server is listening..." << endl;
+
+	while (true)
+	{
+		while (enet_host_service(server, &event, 0) > 0)
+		{
+			switch (event.type)
+			{
+			case ENET_EVENT_TYPE_CONNECT:
+				cout << event.peer->address.host << " has connected!" << endl;
+				peers.push_back(event.peer);
+				break;
+			case ENET_EVENT_TYPE_DISCONNECT:
+				cout << event.peer->address.host << " has disconnected!" << endl;
+				peers.remove(event.peer);
+				break;
+			case ENET_EVENT_TYPE_RECEIVE:
+				SendPackets(event);
+				break;
+			}
+		}
+	}
+}
